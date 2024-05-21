3aa82f629b7fb29c14dba0e217353a3a69561624
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2024-05-21 22:41:38 +0300

    add sixth task

---
+++ Task 6. Motion detection/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace MegaLABS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.loadButton2 = new System.Windows.Forms.Button();
+            this.webcamButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(131, 104);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(400, 360);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(758, 104);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(400, 360);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(902, 48);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(139, 50);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Назад";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // loadButton2
+            // 
+            this.loadButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton2.Location = new System.Drawing.Point(578, 31);
+            this.loadButton2.Name = "loadButton2";
+            this.loadButton2.Size = new System.Drawing.Size(139, 50);
+            this.loadButton2.TabIndex = 12;
+            this.loadButton2.Text = "Загрузить видео";
+            this.loadButton2.UseVisualStyleBackColor = true;
+            this.loadButton2.Click += new System.EventHandler(this.loadButton2_Click);
+            // 
+            // webcamButton
+            // 
+            this.webcamButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.webcamButton.Location = new System.Drawing.Point(578, 104);
+            this.webcamButton.Name = "webcamButton";
+            this.webcamButton.Size = new System.Drawing.Size(139, 50);
+            this.webcamButton.TabIndex = 13;
+            this.webcamButton.Text = "Веб-камера";
+            this.webcamButton.UseVisualStyleBackColor = true;
+            this.webcamButton.Click += new System.EventHandler(this.webcamButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(483, 487);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(139, 50);
+            this.startButton.TabIndex = 14;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(666, 487);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(139, 50);
+            this.stopButton.TabIndex = 15;
+            this.stopButton.Text = "Стоп";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click_1);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1382, 646);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.webcamButton);
+            this.Controls.Add(this.loadButton2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button loadButton2;
+        private System.Windows.Forms.Button webcamButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ Task 6. Motion detection/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.OCR;
+using System.Text.RegularExpressions;
+using Emgu.CV.UI;
+using System.Threading;
+
+namespace MegaLABS
+{
+    public partial class Form1 : Form
+    {
+        //private Image<Bgr, byte> _mainImg = null; //Основная картинка
+        private VideoCapture _capture; //Для захвата видео
+        BackgroundSubtractorMOG2 subtractor;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            StartDisplay();
+            DefaultValues();
+        }
+
+        //Загрузка видео
+        private void LoadVideo(VideoCapture video)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Формат(*.mp4, *.avi, *.wmv, *.gif) | *.mp4; *.avi; *.wmv; *,gif";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    _capture = new VideoCapture(ofd.FileName);
+
+                    if (_capture != null)
+                    {
+                        Mat frame = new Mat();
+                        _capture.Retrieve(frame);
+                        subtractor = new BackgroundSubtractorMOG2(1000, 32, true);
+                        _capture.ImageGrabbed += ProcessFrameVideo;
+                        _capture.Start();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void loadButton2_Click(object sender, EventArgs e)
+        {
+            resetButton.Visible = true;
+            imageBox2.Visible = true;
+            imageBox1.Visible = true;
+            startButton.Visible = true;
+            stopButton.Visible = true;
+            webcamButton.Visible = false;
+
+            LoadVideo(_capture);
+        }
+
+        private void ProcessFrameVideo(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat frame = new Mat();
+                _capture.Retrieve(frame);
+                imageBox1.Image = frame.ToImage<Bgr, byte>().Flip(FlipType.Horizontal).Resize(400, 360, Inter.Linear);
+                imageBox2.Image = ObjectDetection(frame.ToImage<Bgr, byte>(), subtractor).Flip(FlipType.Horizontal).Resize(400, 360, Inter.Linear);
+
+                Thread.Sleep((int)_capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+            }
+            catch (Exception ex)
+            {
+                _capture.Stop();
+            }
+        }
+
+        //Фильтр на основе морфологических операций
+        public static Image<Gray, byte> FilterMask(Image<Gray, byte> mask)
+        {
+            var anchor = new Point(-1, -1);
+            var borderValue = new MCvScalar(1);
+
+            var kernel = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(3, 3), anchor);
+
+            var closing = mask.MorphologyEx(MorphOp.Close, kernel, anchor, 1, BorderType.Default, borderValue);
+            var opening = closing.MorphologyEx(MorphOp.Open, kernel, anchor, 1,BorderType.Default, borderValue);
+
+            var dilation = opening.Dilate(7);
+
+            var threshold = dilation.ThresholdBinary(new Gray(240), new Gray(255));
+
+            return threshold;
+        }
+
+        //Обнаружение и отрисовка
+        public static Image<Bgr, byte> ObjectDetection(Image<Bgr, byte> frame, BackgroundSubtractorMOG2 subtractor)
+        {
+            var grayFrame = frame.Convert<Gray, byte>();
+            var foregroundMask = grayFrame.CopyBlank();
+
+            subtractor.Apply(grayFrame, foregroundMask);
+            var foregroundMaskFiltered = FilterMask(foregroundMask);
+
+            CvInvoke.Threshold(foregroundMaskFiltered, foregroundMaskFiltered, 200, 240, ThresholdType.Binary);
+            CvInvoke.MorphologyEx(foregroundMaskFiltered, foregroundMaskFiltered, MorphOp.Close, Mat.Ones(7, 3, DepthType.Cv8U, 1), new Point(-1, -1), 1, BorderType.Reflect, new MCvScalar(0));
+
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(foregroundMaskFiltered, contours, null, RetrType.External, ChainApproxMethod.ChainApproxTc89L1);
+
+            int minArea = 2500;
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i]) > minArea)
+                {
+                    Rectangle boundingRect = CvInvoke.BoundingRectangle(contours[i]);
+                    CvInvoke.Rectangle(frame, boundingRect, new MCvScalar(0, 255, 0), 2);
+                }
+            }
+
+            return frame;
+        }
+
+        //Веб камера
+        private void webcamButton_Click(object sender, EventArgs e)
+        {
+            imageBox1.Visible = true;
+            imageBox2.Visible = true;
+            resetButton.Visible = true;
+            startButton.Visible = true;
+            stopButton.Visible = true;
+            loadButton2.Visible = false;
+
+            _capture = new VideoCapture();
+
+            if (_capture != null)
+            {
+                subtractor = new BackgroundSubtractorMOG2(1000, 32, true);
+                _capture.ImageGrabbed += ProcessFrameVideo;
+                _capture.Start();
+            }
+        }
+
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            _capture.Start();
+        }
+
+        private void stopButton_Click_1(object sender, EventArgs e)
+        {
+            _capture.Stop();
+        }
+
+        //Интерфейс
+        private void ComponentsVisibleFalse()
+        {
+            //нет элементов
+        }
+
+        private void ButtonsVisibleFalse()
+        {
+            resetButton.Visible = false;
+            startButton.Visible = false;
+            stopButton.Visible = false;
+        }
+
+        private void DefaultValues()
+        {
+            //нет значений
+        }
+
+        private void StartDisplay()
+        {
+            webcamButton.Visible = true;
+            loadButton2.Visible = true;
+            ComponentsVisibleFalse();
+            ButtonsVisibleFalse();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            StartDisplay();
+            DefaultValues();
+            imageBox2.Image = null;
+            imageBox1.Image = null;
+            imageBox2.Visible = false;
+            imageBox1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

e4043f84468a092a318fd5bd1f4ffad65fb1804e
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2024-05-21 22:41:21 +0300

    add fifth task

---
+++ Task 5. Text and faces on image/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace MegaLABS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.loadButton1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.contoursButtonText = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loadButton2 = new System.Windows.Forms.Button();
+            this.contoursButtonFaces = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton1
+            // 
+            this.loadButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton1.Location = new System.Drawing.Point(469, 25);
+            this.loadButton1.Name = "loadButton1";
+            this.loadButton1.Size = new System.Drawing.Size(139, 50);
+            this.loadButton1.TabIndex = 0;
+            this.loadButton1.Text = "Загрузить изображение";
+            this.loadButton1.UseVisualStyleBackColor = true;
+            this.loadButton1.Click += new System.EventHandler(this.loadButton1_Click_1);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(131, 104);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(400, 360);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(758, 104);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(400, 360);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(902, 48);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(139, 50);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Назад";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // contoursButtonText
+            // 
+            this.contoursButtonText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.contoursButtonText.Location = new System.Drawing.Point(567, 104);
+            this.contoursButtonText.Name = "contoursButtonText";
+            this.contoursButtonText.Size = new System.Drawing.Size(162, 50);
+            this.contoursButtonText.TabIndex = 10;
+            this.contoursButtonText.Text = "Выделить текст";
+            this.contoursButtonText.UseVisualStyleBackColor = true;
+            this.contoursButtonText.Click += new System.EventHandler(this.contoursButtonText_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(821, 481);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 24);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // loadButton2
+            // 
+            this.loadButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton2.Location = new System.Drawing.Point(687, 25);
+            this.loadButton2.Name = "loadButton2";
+            this.loadButton2.Size = new System.Drawing.Size(139, 50);
+            this.loadButton2.TabIndex = 12;
+            this.loadButton2.Text = "Загрузить видео";
+            this.loadButton2.UseVisualStyleBackColor = true;
+            this.loadButton2.Click += new System.EventHandler(this.loadButton2_Click);
+            // 
+            // contoursButtonFaces
+            // 
+            this.contoursButtonFaces.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.contoursButtonFaces.Location = new System.Drawing.Point(567, 171);
+            this.contoursButtonFaces.Name = "contoursButtonFaces";
+            this.contoursButtonFaces.Size = new System.Drawing.Size(162, 50);
+            this.contoursButtonFaces.TabIndex = 13;
+            this.contoursButtonFaces.Text = "Обнаружение лиц";
+            this.contoursButtonFaces.UseVisualStyleBackColor = true;
+            this.contoursButtonFaces.Click += new System.EventHandler(this.contoursButtonFaces_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(567, 240);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(162, 50);
+            this.startButton.TabIndex = 14;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(567, 305);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(162, 50);
+            this.stopButton.TabIndex = 15;
+            this.stopButton.Text = "Стоп";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1382, 646);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.contoursButtonFaces);
+            this.Controls.Add(this.loadButton2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.contoursButtonText);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.loadButton1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button contoursButtonText;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button loadButton2;
+        private System.Windows.Forms.Button contoursButtonFaces;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ Task 5. Text and faces on image/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.OCR;
+using System.Text.RegularExpressions;
+using Emgu.CV.UI;
+using System.Threading;
+
+
+namespace MegaLABS
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> _mainImg = null; //Основная картинка
+        private VideoCapture _capture; //Для захвата видео
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            StartDisplay();
+            DefaultValues();
+        }
+
+        //Загрузка изображения
+        private void LoadImage(Image<Bgr, byte> img)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Формат(*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    img = new Image<Bgr, byte>(ofd.FileName);
+                    _mainImg = img;
+                    imageBox1.Image = img.Resize(400, 360, Inter.Linear);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Загрузка видео
+        private void LoadVideo(VideoCapture video)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Формат(*.mp4, *.avi, *.wmv, *.gif) | *.mp4; *.avi; *.wmv; *,gif";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    _capture = new VideoCapture(ofd.FileName);
+                    _capture.ImageGrabbed += ProcessFrameVideo;
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void loadButton1_Click_1(object sender, EventArgs e)
+        {
+            LoadImage(_mainImg);
+        }
+
+        private void loadButton2_Click(object sender, EventArgs e)
+        {
+            resetButton.Visible = true;
+            startButton.Visible = true;
+            stopButton.Visible = true;
+            contoursButtonFaces.Visible = false;
+            contoursButtonText.Visible = false;
+            textBox1.Visible = false;
+            LoadVideo(_capture);
+            _capture.Start();
+        }
+
+        //Распознавание символов
+        public StringBuilder CharacterRecognition(Image<Bgr, byte> img, VectorOfVectorOfPoint contours)
+        {
+            var strBuilder = new StringBuilder();
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i], false) > 50)
+                {
+                    Rectangle rectangle = CvInvoke.BoundingRectangle(contours[i]);
+                    img.ROI = rectangle;
+
+                    Image<Bgr, byte> roiImg;
+                    roiImg = img.Clone();
+                    img.ROI = Rectangle.Empty;
+
+                    var _ocr = new Tesseract("D:\\tessdata-main", "eng", OcrEngineMode.TesseractLstmCombined);
+                    _ocr.SetImage(roiImg);
+                    _ocr.Recognize();
+
+                    Tesseract.Character[] words = _ocr.GetCharacters();
+
+                    for (; i < words.Length; i++)
+                    {
+                        strBuilder.Append(words[i].Text);
+                    }
+                }
+            }
+
+            return strBuilder;
+        }
+
+        //Выделение участков текста
+        private Image<Bgr, byte> ContoursText(Image<Bgr, byte> img)
+        {
+            var grayImg = img.Clone().Convert<Gray, byte>();
+            grayImg._ThresholdBinaryInv(new Gray(128), new Gray(255));
+            grayImg._Dilate(4);
+
+            var contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(grayImg, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+            var resultImg = img.Copy();
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i], false) > 50)
+                {
+                    Rectangle rectangle = CvInvoke.BoundingRectangle(contours[i]);
+                    resultImg.Draw(rectangle, new Bgr(Color.Yellow), 2);
+                }
+            }
+
+            var ocrFromContours = CharacterRecognition(img, contours);
+            textBox1.Text = ocrFromContours.ToString();
+
+            return resultImg;
+        }
+
+        private void contoursButtonText_Click(object sender, EventArgs e)
+        {
+            if (imageBox1.Image != null)
+            {
+                textBox1.Visible = true;
+                resetButton.Visible = true;
+                imageBox2.Image = ContoursText(_mainImg).Resize(400, 360, Inter.Linear);
+            }
+            else
+            {
+                MessageBox.Show("Выберите изображение!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //Обнаружение лиц
+        public Image<Bgr, byte> FaceDetect(Image<Bgr, byte> img)
+        {
+            var resultImg = img.Clone();
+
+            List<Rectangle> faces = new List<Rectangle>();
+
+            using (CascadeClassifier face = new CascadeClassifier("D:\\opencv-master\\data\\haarcascades\\haarcascade_frontalface_default.xml"))
+            {
+                using (Mat ugray = new Mat())
+                {
+                    CvInvoke.CvtColor(img, ugray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                    Rectangle[] facesDetected = face.DetectMultiScale(ugray, 1.1, 10, new Size(20, 20));
+
+                    faces.AddRange(facesDetected);
+                }
+            }
+
+            foreach (Rectangle rectangle in faces)
+            {
+                resultImg.Draw(rectangle, new Bgr(Color.Yellow), 2);
+            }
+
+            return resultImg;
+        }
+
+        private void ProcessFrameVideo(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat frame = new Mat();
+                _capture.Retrieve(frame);
+
+                var frameImg = frame.ToImage<Bgr, byte>();
+                imageBox1.Image = frameImg.Resize(400, 360, Inter.Linear);
+                imageBox2.Image = FaceDetect(frameImg).Resize(400, 360, Inter.Linear);
+
+                Thread.Sleep((int)_capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+            }
+            catch (Exception ex)
+            {
+                _capture.Stop();
+            }
+        }
+
+        private void contoursButtonFaces_Click(object sender, EventArgs e)
+        {
+            if (imageBox1.Image != null)
+            {
+                textBox1.Visible = false;
+                resetButton.Visible = true;
+                imageBox2.Image = FaceDetect(_mainImg).Resize(400, 360, Inter.Linear);
+            }
+            else
+            {
+                MessageBox.Show("Выберите изображение!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            _capture.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            _capture.Stop();
+        }
+
+        //Интерфейс
+        private void ComponentsVisibleFalse()
+        {
+            textBox1.Visible = false;
+        }
+
+        private void ButtonsVisibleFalse()
+        {
+            resetButton.Visible = false;
+            startButton.Visible = false;
+            stopButton.Visible = false;
+        }
+
+        private void DefaultValues()
+        {
+
+        }
+
+        private void StartDisplay()
+        {
+            contoursButtonText.Visible = true;
+            contoursButtonFaces.Visible = true;
+            ComponentsVisibleFalse();
+            ButtonsVisibleFalse();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            StartDisplay();
+            DefaultValues();
+            resetButton.Visible = false;
+            imageBox2.Image = null;
+            textBox1.Text = null;
+            textBox1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
